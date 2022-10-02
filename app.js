54c3922eed9a582b51cecbe4db23edc98ff64ee7
moneym4st3r <101295961+moneym4st3r@users.noreply.github.com>
2022-10-02 14:02:04 +0300

    commit

--- app.js
+++ app.js
@@ -8,10 +8,38 @@ function getData(pairList) {
         dataType: 'json',
         success: function(data) {
             console.log(data);
-            render(data, pairList)
+            parse(data, pairList)
         }
     });
 
+}
+function getSymbols(symbols) {
+    let endpoint = 'https://api.binance.com/api/v3/exchangeInfo';
+    $.ajax({
+        url: endpoint,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            let psymbols = [];
+            data.symbols.forEach(function(symbol) {
+                symbols.forEach(function(pair) {
+                    if (symbol.symbol.toString().toUpperCase() === pair.toString().toUpperCase()) {
+                        psymbols.push(symbol);
+                    }
+                });
+            });
+            let bsymbols = [];
+            psymbols.forEach(function(symbol) {
+                let mintick = symbol.filters[0].tickSize;
+                bsymbols.push({
+                    symbol: symbol.symbol,
+                    minTick: parseFloat(mintick)
+                });
+            });
+            localStorage.setItem('symbols_minticks', JSON.stringify(bsymbols));
+        }
+
+    });
 }
 
 /* Global var for counter */
@@ -29,8 +57,10 @@ function fnClickAddRow(tableData) {
 	$('#example').dataTable().fnAddData( [
         pair.symbol,
         pair.price,
+        pair.p_vol,
         pair.Dvolatility,
-        pair.stringofDigits,
+        pair.change,
+        pair.min_tick,
         pair.bppop,
     ] );
 }
@@ -53,44 +83,34 @@ function cleanFloat(num) {
     return newNum;
 }
 
-function render(data, pairList) {
+function parse(data, pairList) {
+    getSymbols(pairList);
     let html = "";
     let tabledata = [];
     data.forEach(function(pair) {
         pairList.forEach(function(pairName) {
             if (pair.symbol == pairName.toUpperCase()) {
-                let stringofDigits = '';
+                let min_tick = JSON.parse(localStorage.getItem('symbols_minticks')).find(function(symbol) {
+                    return symbol.symbol == pair.symbol;
+                }).minTick;
+                console.log("------------------", min_tick);
                 let symbol = pair.symbol;
                 let price = pair.lastPrice;
                 let volume = pair.volume;
-                let Dvolatility = pair.priceChangePercent;
-                if(Dvolatility < 0){
-                    Dvolatility*= -1;
-                }
+                let p_vol = pair.highPrice-pair.lowPrice;
                 let vol = volume;
-                let leftright = parseFloat(price).toString().split('.');
-                if (parseFloat(price).toString().includes('.')) {
+                let bppop = ((1/parseFloat(price))*parseFloat(min_tick)*100)+"%"
+                let ppbp = (parseFloat(price) - parseFloat(min_tick)) / parseFloat(min_tick);
+                let volpppbp = parseFloat(volume) / parseFloat(ppbp);
+                let pp_vol =  parseFloat(volume)/parseFloat(price) ;
+                let pp_volpppbp = parseFloat(pp_vol)/parseFloat(ppbp);
+                let Dpvolatility = parseFloat(p_vol)/parseFloat(min_tick);
+                let Dvolatility = parseFloat(Dpvolatility) * parseFloat(bppop);
+                let change = pair.priceChangePercent;
 
-                    let left = leftright[0];
-                    let right = leftright[1];
-                    let digitNo = 0;
-                    if (right.length == null) {} else {
-                        digitNo = right.length;
-                    }
-                    stringofDigits += '0.';
-                    for (let i = 0; i < digitNo - 1; i++) {
-                        stringofDigits += '0';
-                    }
-
-                }
-                stringofDigits += '1';
-                let bppop = ((1/parseFloat(price))*parseFloat(stringofDigits)*100).toFixed(3)+"%"
-                let ppbp = (parseFloat(price) - parseFloat(stringofDigits)) / parseFloat(stringofDigits);
-                let volpppbp = parseFloat(volume) / ppbp;
-                let ppvol =  parseFloat(volume)/parseFloat(price) ;
-                let ppvolpppbp = ppvol/ppbp;
-                html += '<tr><td>' + symbol + '</td><td>' + parseFloat(price) + '</td><td>' + parseFloat(stringofDigits) + '</td><td>'+parseFloat(bppop)+'</td></tr>';
-                tabledata.push({ "symbol": symbol, "price": parseFloat(price),"Dvolatility":parseFloat( Dvolatility)+"%", "stringofDigits": parseFloat(stringofDigits), "ppbp": parseInt(ppbp), "volume": parseFloat(vol), "volpppbp": parseFloat(volpppbp).toFixed(2), "ppvol": parseFloat(ppvol).toFixed(2), "ppvolpppbp": parseFloat(ppvolpppbp).toFixed(2) , "bppop": bppop});
+                console.log("high: "+pair.highPrice+"\nlow: "+pair.lowPrice+"\np_vol: "+p_vol, "\nmin_tick: "+min_tick, "\nDpvolatility: "+Dpvolatility);
+                html += '<tr><td>' + symbol + '</td><td>' + parseFloat(price) + '</td><td>' + parseFloat(min_tick) + '</td><td>'+parseFloat(bppop)+'</td></tr>';
+                tabledata.push({ "symbol": symbol, "price": parseFloat(price),"p_vol":parseInt(Dpvolatility),"Dvolatility":parseFloat(Dvolatility)+"%","change":parseFloat(change)+"%", "min_tick": parseFloat(min_tick), "ppbp": parseInt(ppbp), "volume": parseFloat(vol), "volpppbp": parseFloat(volpppbp).toFixed(2), "pp_vol": parseFloat(pp_vol).toFixed(2), "pp_volpppbp": parseFloat(pp_volpppbp).toFixed(2) , "bppop": bppop});
                 console.log(ppbp)
             }
         });
@@ -101,6 +121,12 @@ function render(data, pairList) {
 };
 
 
+
+function render(data) {
+
+
+
+}
 
 $(document).ready(function() {
     $("#example").DataTable();
@@ -121,3 +147,7 @@ var getUrlParameter = function getUrlParameter(sParam) {
     }
     return false;
 };
+
+
+
+;
--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 
 This is a simple script to calculate the percentage variation per minimum tick of a given pair.
 
+## Usage
+add query parameter pairs with ; between values
+
+**example:** https://mihailnicamn.github.io/profitpertick-screener/?pairs=BTCUSDT;OPUSDT 
